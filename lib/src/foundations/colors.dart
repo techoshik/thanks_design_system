13915323ff1f4bf158a1f25cf5b2943b3f05c3c2
Hemant Kaushik <TheHemantKaushik@gmail.com>
2026-09-05 17:34:17 +0530

    style: update pageBackground color to 0xFFF7F8FA

--- lib/src/foundations/colors.dart
+++ lib/src/foundations/colors.dart
@@ -11,7 +11,7 @@ abstract final class ThanksColors {
   static const primary800 = Color(0xFF0D2278);
   static const primary950 = Color(0xFF060F44);
 
-  static const pageBackground = Color(0xFFF7F6F3);
+  static const pageBackground = Color(0xFFF7F8FA);
   static const border = Color(0xFFE2DFD8);
   static const borderStrong = Color(0xFFC8C4BB);
 
